772dc34c7f72b5adef617a9700b5608066f6fc6f
vitalika1234 <vitaliihoncharuk@gmail.com>
2020-09-07 16:22:40 +0300

    fix code formatting

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,13 +1,11 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 try {
+  const name = core.getInput('who-to-greet');
+  console.log(`Hello , ${name} !`);
 
+  const time = new Date();
+  core.setOutput('time', time.toString());
 } catch (error) {
-  core.setFailed(error.message)
+  core.setFailed(error.message);
 }
-const name= core.getInput('who-to-greet');
-console.log(`Hello , ${name} !`);
-
-const time = new Date();
-core.setOutput('time', time.toString());
