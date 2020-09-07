05b21471db475a740822a81ea06112dedb9cf184
vitalika1234 <vitaliihoncharuk@gmail.com>
2020-09-07 16:19:49 +0300

    add action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,13 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+
+} catch (error) {
+  core.setFailed(error.message)
+}
+const name= core.getInput('who-to-greet');
+console.log(`Hello , ${name} !`);
+
+const time = new Date();
+core.setOutput('time', time.toString());
